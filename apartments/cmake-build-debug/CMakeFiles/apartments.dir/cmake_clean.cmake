4335a701bedb4fd0822f0c1ebb945a737e8c61a4
akanerva <aleksi.kanerva90@gmail.com>
2021-12-31 02:12:13 +0200

    ignore test

---
+++ apartments/cmake-build-debug/CMakeFiles/apartments.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/apartments.dir/main.cpp.o"
+  "apartments.exe"
+  "apartments.pdb"
+  "libapartments.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/apartments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
